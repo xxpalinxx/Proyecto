5dd7f026a9b6d879d6cbf5c204bd5e400d8654cb
xxpalinxx <hkpalin@gmail.com>
2024-10-02 11:01:21 -0300

    adaptando el proyecto a custom hook

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,71 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useCart } from './hooks/useCart'
 import Header from './components/Header'
 import Guitar from './components/Guitar'
-import { db } from './data/db'
 import './App.css'
 
 function App() {
-    const initialCart = () => {
-        const localStorageCart = localStorage.getItem('cart')
-        return localStorageCart ? JSON.parse(localStorageCart) : []
-    }
-
-    const [data] = useState(db)
-    const [cart, setCart] = useState(initialCart)
-
-    const MIN_ITEMS = 1
-    const MAX_ITEMS = 5
     
-    useEffect(() => {
-        localStorage.setItem('cart',JSON.stringify(cart))
-    }, [cart])
-
-    function addToCart (item) {
-        const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
-        if(itemExist >=0) { //Significa que el item ya existe
-            if(cart[itemExist].quantity >= MAX_ITEMS) return
-            const updatedCart = [...cart]
-            updatedCart[itemExist].quantity++
-            setCart(updatedCart)
-        } else {
-            item.quantity = 1
-            setCart([...cart, item])
-        }
-    }
-
-    function removeFromCart(id) {
-        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
-    }
-
-    function increaseQuantity (id) {
-        const updatedCart = cart.map(item => {
-            if(item.id === id && item.quantity < MAX_ITEMS) {
-                return {
-                    ...item,
-                    quantity: item.quantity +1
-                }
-            }
-            return item
-        })
-        setCart(updatedCart)
-    }
-
-    function decreaseQuantity (id) {
-        const updatedCart = cart.map(item => {
-            if(item.id === id && item.quantity > MIN_ITEMS){
-                return {
-                    ...item,
-                    quantity: item.quantity -1
-                }
-            }
-            return item
-        })
-        setCart(updatedCart)
-    }
-
-    function clearCart () {
-        setCart([])
-    }
+    const {data,
+            cart,
+            addToCart,
+            removeFromCart,
+            decreaseQuantity,
+            increaseQuantity,
+            clearCart,
+            isEmpty,
+            cartTotal} = useCart()
 
     return (
         <>
@@ -75,6 +23,8 @@ function App() {
             increaseQuantity={increaseQuantity}
             decreaseQuantity={decreaseQuantity}
             clearCart={clearCart}
+            isEmpty={isEmpty}
+            cartTotal={cartTotal}
         />
 
         <main className="container-xl mt-5">
@@ -85,7 +35,6 @@ function App() {
                     <Guitar
                         key={guitar.id}
                         guitar={guitar}
-                        setCart={setCart}
                         addToCart={addToCart}
                     />
                     )}
--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,11 +1,7 @@
-import { useMemo } from "react"
+import { useCart } from "../hooks/useCart"
 
+function Header ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal}) {
 
-function Header ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart}) {
-
-    //State derivado
-    const isEmpty = useMemo( () => cart.length === 0, [cart])
-    const cartTotal = useMemo(() => cart.reduce((total, item) => total + item.quantity * item.price, 0),[cart])
 
     return (
         <header className="py-5 header">
