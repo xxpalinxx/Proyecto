6104aca0391f01f79a68f7f7d98ea4d58bbe8830
xxpalinxx <hkpalin@gmail.com>
2024-10-04 22:11:48 -0300

    creando proyecto de control de gastos con React y ContextAPI

---
+++ control-gastos-contextapi/src/App.tsx
@@ -0,0 +1,22 @@
+import BudgeForm from "./components/BudgeForm"
+
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Control de Gastos
+        </h1>
+      </header>
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgeForm 
+        
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
